ef9535c1ac5138f948bab4bb5364cff2cccde8dd
Nkeiru Lois <nkeiruloisy@yahoo.com>
2025-11-09 16:29:32 +0100

    task 7 update

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -61,7 +61,7 @@ console.log(executeWork(createEmployee(1000)));
 
 type Subjects = 'Math' | 'History';
 
-function teachClass(todayClass: Subjects): string {
+function teachClass(todayClass:Subjects): string {
     if (todayClass === 'Math') {
         return 'Teaching Math';
     }
