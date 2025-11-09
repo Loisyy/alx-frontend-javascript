e372de8d20917b39368a194516881dce4039fc01
Nkeiru Lois <nkeiruloisy@yahoo.com>
2025-11-09 16:25:32 +0100

    commit task 6

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -49,7 +49,7 @@ export function isDirector(employee: Director | Teacher): employee is Director {
     return employee instanceof Director;
 }
 
-function executeWork(employee: Director | Teacher): string {
+export function executeWork(employee: Director | Teacher): string {
     if (isDirector(employee)) {
         return employee.workDirectorTasks();
     }
