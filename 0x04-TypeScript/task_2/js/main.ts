8780eec4e48c97c24ee21bf55036f1dfccd29e66
Nkeiru Lois <nkeiruloisy@yahoo.com>
2025-11-09 16:08:10 +0100

    update for failed task

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,71 +1,70 @@
+
 interface DirectorInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
 }
 
 interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
 }
 
 class Director implements DirectorInterface {
-  workFromHome(): string {
-    return "Working from home";
-  }
+    workFromHome(): string {
+        return 'Working from home';
+    }
 
-  getCoffeeBreak(): string {
-    return "Getting a coffee break";
-  }
+    getCoffeeBreak(): string {
+        return 'Getting a coffee break';
+    }
 
-  workDirectorTasks(): string {
-    return "Getting to director tasks";
-  }
+    workDirectorTasks(): string {
+        return 'Getting to director tasks';
+    }
 }
 
 class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return 'Cannot work from home';
-  } 
-  
-  getCoffeeBreak(): string {
-    return 'Cannot have a break';
-  } 
-  
-  workTeacherTasks(): string {
-    return 'Getting to work';
-  } 
+    workFromHome(): string {
+        return 'Cannot work from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Cannot have a break';
+    }
+
+    workTeacherTasks(): string {
+        return 'Getting to work';
+    }
 }
 
-type Employee = Director | Teacher;
-
-function createEmployee(salary: number | string): Employee {
-  if (salary < 500) {
-    return new Teacher();
-  }
-  return new Director();
+function createEmployee(salary: number | string): Teacher | Director {
+    return typeof salary === 'number' && salary < 500 ? new Teacher() : new Director();
 }
 
-function isDirector(employee: Employee): employee is Director {
-  return employee instanceof Director;
-}
+console.log(createEmployee(200));
 
-function executeWork(employee: Employee): string {
-  if (isDirector(employee)) {
-    return employee.workDirectorTasks(); 
-  }
-  return employee.workTeacherTasks();
-}
+console.log(createEmployee(1000));
 
-type Subjects = "Math" | "History";
+console.log(createEmployee('$500'));
 
-function teachClass(todayClass: Subjects): string {
-  if (todayClass === "Math") {
-    return "Teaching Math";
-  }
-  return "Teaching History";
-}
+const isDirector = (employee: Teacher | Director): boolean => employee instanceof Director;
+
+const executeWork = (employee: Teacher | Director): string => {
+    let res;
+    isDirector(employee) ? res = (employee as Director).workDirectorTasks() : res = (employee as Teacher).workTeacherTasks();
+    return res;
+};
 
 console.log(executeWork(createEmployee(200)));
+
 console.log(executeWork(createEmployee(1000)));
+
+type Subjects = 'Math' | 'History';
+
+const teachClass = (todayClass: Subjects): string => todayClass === 'Math' ? 'Teaching Math' : 'Teaching History';
+
+console.log(teachClass('Math'));
+
+console.log(teachClass('History'));
